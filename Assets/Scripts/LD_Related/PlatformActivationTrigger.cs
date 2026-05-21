4165f5227bf7feca95da976230fd8a7e19a483a8
Ozinor75 <f.crumois@myskolae.fr>
2026-05-21 20:03:29 +0200

    Patrol ++

---
+++ Assets/Scripts/LD_Related/PlatformActivationTrigger.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+public class PlatformActivationTrigger : MonoBehaviour
+{
+    public PlatformMovement movable;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+            movable.canMove = true;
+    }
+}
